88a4a6ea9959269f1581348b6a5fee72f42b62b5
Dravid Joseph <dravidjoseph@gmail.com>
2016-02-22 21:57:53 -0600

    Add backend

--- Ex2/Quiz.php
+++ Ex2/Quiz.php
@@ -1,9 +1,30 @@
 <?php
-//access the global array called $_POST to get the values from the text fields
-$name = $_POST["name"];
-$email = $_POST["email"];
+//access the global array called $_POST to get the values from the radio fields
+$q1 = $_POST["moldova"];
+$q2 = $_POST["nepal"];
+$q3 = $_POST["ghana"];
+$q4 = $_POST["honduras"];
+$q5 = $_POST["uruguay"];
 
-echo "Name: " . $name . "<br>";
-echo "Email: " . $email . "<br>";
+$score = 0;
+
+if(q1 == "Chisinau"){
+	++score;
+}
+if(q2 == "Kathmandu"){
+	++score;
+}
+if(q3 == "Accra"){
+	++score;
+}
+if(q4 == "Tegucigalpa"){
+	++score;
+}
+if(q5 == "Montevideo"){
+	++score;
+}
+
+echo '<br> Your score is '.(score/5)*100.'%<br>';
+
 
 ?>
