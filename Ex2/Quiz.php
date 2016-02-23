cca394836f82f8cec873276e3c2b025132d8b083
Dravid Joseph <dravidjoseph@gmail.com>
2016-02-22 22:30:23 -0600

    Add question validation

--- Ex2/Quiz.php
+++ Ex2/Quiz.php
@@ -6,11 +6,21 @@ $q3 = $_POST["ghana"];
 $q4 = $_POST["honduras"];
 $q5 = $_POST["uruguay"];
 
-echo $q1."<br>";
-echo $q2."<br>";
-echo $q3."<br>";
-echo $q4."<br>";
-echo $q5."<br>";
+echo "Question 1: What is the capital of Moldova?<br>";
+echo "You answered ".$q1."<br>";
+echo "Correct answer: Chisinau<br>";
+echo "Question 2: What is the capital of Nepal?<br>";
+echo "You answered ".$q2."<br>";
+echo "Correct answer: Kathmandu<br>";
+echo "Question 3: What is the capital of Ghana?<br>";
+echo "You answered ".$q3."<br>";
+echo "Correct answer: Accra<br>";
+echo "Question 4: What is the capital of Honduras?<br>";
+echo "You answered ".$q4."<br>";
+echo "Correct answer: Tegucigalpa<br>";
+echo "Question 5: What is the capital of Uruguay?<br>";
+echo "You answered ".$q5."<br>";
+echo "Correct answer: Montevideo<br>";
 
 $score = 0;
 
@@ -32,7 +42,7 @@ if($q5 == "Montevideo"){
 $score = $score * 20;
 echo $score;
 
-echo "<br> Your score is ".$score."%%";
+echo "<br> Your score is ".$score."%";
 
 
 ?>
