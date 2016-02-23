694fe7e8a80894249a6294249745de787cbcb898
Dravid Joseph <dravidjoseph@gmail.com>
2016-02-22 22:32:23 -0600

    Add spacing

--- Ex2/Quiz.php
+++ Ex2/Quiz.php
@@ -7,19 +7,19 @@ $q4 = $_POST["honduras"];
 $q5 = $_POST["uruguay"];
 
 echo "Question 1: What is the capital of Moldova?<br>";
-echo "You answered ".$q1."<br>";
-echo "Correct answer: Chisinau<br>";
+echo "You answered: ".$q1."<br>";
+echo "Correct answer: Chisinau<br><br>";
 echo "Question 2: What is the capital of Nepal?<br>";
-echo "You answered ".$q2."<br>";
+echo "You answered: ".$q2."<br><br>";
 echo "Correct answer: Kathmandu<br>";
 echo "Question 3: What is the capital of Ghana?<br>";
-echo "You answered ".$q3."<br>";
+echo "You answered: ".$q3."<br><br>";
 echo "Correct answer: Accra<br>";
 echo "Question 4: What is the capital of Honduras?<br>";
-echo "You answered ".$q4."<br>";
+echo "You answered: ".$q4."<br><br>";
 echo "Correct answer: Tegucigalpa<br>";
 echo "Question 5: What is the capital of Uruguay?<br>";
-echo "You answered ".$q5."<br>";
+echo "You answered: ".$q5."<br><br>";
 echo "Correct answer: Montevideo<br>";
 
 $score = 0;
@@ -40,9 +40,8 @@ if($q5 == "Montevideo"){
 	++$score;
 }
 $score = $score * 20;
-echo $score;
 
-echo "<br> Your score is ".$score."%";
+echo "<br><br> Your score is ".$score."%";
 
 
 ?>
