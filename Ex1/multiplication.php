d233e00e9bfccb355b117a77c8f0a66179d580c3
Dravid Joseph <dravidjoseph@gmail.com>
2016-02-22 19:19:52 -0600

    Initial commit multiplication talble

---
+++ Ex1/multiplication.php
@@ -0,0 +1,21 @@
+<?php
+
+
+	echo '<table border = '1' style = 'width = 100%'>';
+	//first row manually
+	echo '<tr>';
+	for($i = 1; $i <=10; ++$i){
+		echo '<td>'.$i.'</td>';
+	}
+	echo '</tr>';
+	
+	for($i = 1; $i <= 10; ++i){
+		echo '<td>'.$i.'</td>';
+		for($j = 1; j <= 10; ++j){
+			echo '<td>'.$i * $j.'</td>';
+		}
+	}
+	
+	echo '</table>';
+	
+?>
